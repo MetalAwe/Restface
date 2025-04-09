5fc89957039b72034f45cebcb40e93a064717631
MetalAwe <133659786+MetalAwe@users.noreply.github.com>
2025-04-09 11:21:27 +0300

    version 0.4

--- BonFiscal.vb
+++ BonFiscal.vb
@@ -1,16 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 
 Public Class BonFiscal
-    Private Sub TextBoxMetodaPlata_TextChanged(sender As Object, e As EventArgs) Handles TextBoxMetodaPlata.TextChanged
-
-    End Sub
-
     Private Sub BonFiscal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim RadioButtonsData As Boolean = TableInterface.RadioButtonCard.Checked
-        If RadioButtonsData = False Then
-            TextBoxMetodaPlata.Text = "Cash"
-        Else
-            TextBoxMetodaPlata.Text = "Card"
-        End If
+
     End Sub
 End Class
